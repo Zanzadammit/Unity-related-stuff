6e00aef0fd8db80c269832fb5084ab70be818ba8
Zanzadammit <123448030+Zanzadammit@users.noreply.github.com>
2023-01-24 09:41:24 +0100

    initcom

---
+++ GitHubStuff/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) rb.velocity = Vector2.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) rb.velocity = Vector2.left * speed * Time.deltaTime;
+    }
+}
